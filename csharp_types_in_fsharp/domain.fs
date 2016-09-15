45fa6f1d17895dbe99e42ba8b5c4a4e9e86f2f8a
simax <simax.99.99@gmail.com>
2016-09-15 08:01:47 +0100

    Initial commit

---
+++ csharp_types_in_fsharp/domain.fs
@@ -0,0 +1,17 @@
+﻿
+namespace CsharpTypesInFsharp
+ 
+module domain =
+
+    type Customer(customerId:string, name:string) = 
+
+        let mutable customerName = name 
+        member x.CustomerId = customerId 
+        member x.Name 
+            with get() = customerName
+            and set(value) = customerName <- value
+             
+
+        
+
+
